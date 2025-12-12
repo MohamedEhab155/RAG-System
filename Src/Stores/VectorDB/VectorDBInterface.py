680598b3322b50d78b8d3ebca37d55079f72125a
MohamedEhab155 <ma0587580@gmail.com>
2025-12-12 13:32:09 +0200

    Update search_by_vector method signature to include return type annotation

--- Src/Stores/VectorDB/VectorDBInterface.py
+++ Src/Stores/VectorDB/VectorDBInterface.py
@@ -1,6 +1,6 @@
 from abc import ABC , abstractmethod 
-
-
+from Models.db_Schema.ChunkDtata import RetrievedDocument
+from typing import List
 class VectorDBInterface(ABC): 
     
 
@@ -37,7 +37,7 @@ class VectorDBInterface(ABC):
         pass
 
     @abstractmethod
-    def search_by_vector(self, collection_name: str, vector: list, limit: int):
+    def search_by_vector(self, collection_name: str, vector: list, limit: int)->List [RetrievedDocument]:
         pass
     @abstractmethod
     def get_collection_info(self, collection_name: str) -> dict:
