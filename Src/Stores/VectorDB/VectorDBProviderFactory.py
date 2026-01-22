8980df4404cfbca908144e343306ab7d6d22b8fa
MohamedEhab155 <ma0587580@gmail.com>
2026-01-22 14:30:40 +0200

    feat: update VectorDBProviderFactory to include db_client parameter and enhance provider creation logic

--- Src/Stores/VectorDB/VectorDBProviderFactory.py
+++ Src/Stores/VectorDB/VectorDBProviderFactory.py
@@ -1,21 +1,28 @@
 from .Providers.QdrantProviders import QdrantProviders
+from.Providers.PgVectorProviders import PgVectorProviders
 from .VectorDBEnums import VectorDBEnums
 from Contoroller.BaseContoroller import BaseContoroller
-
+from sqlalchemy.orm import sessionmaker
 
 class VectorDBProviderFactory:
-    def __init__(self,config):
+    def __init__(self,config,db_client:sessionmaker=None):
             self.config = config
             self.base_controller = BaseContoroller()
-    
+            self.db_client = db_client
 
     def create(self, provider: str):
         if provider == VectorDBEnums.QDRANT.value:
-            db_path = self.base_controller.get_db_path(provider_name=self.config.VECTOR_DB_PATH)
+            qdrant_db_client = self.base_controller.get_db_path(provider_name=self.config.VECTOR_DB_PATH)
 
             return QdrantProviders(
-                db_path=db_path,
+                db_path=qdrant_db_client,
                 distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
             )
-        
-        return None
+        if provider == VectorDBEnums.PGVECTOR.value:
+            return PgVectorProviders(
+                db_client=self.db_client,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                default_vector_size=self.config.DEFAULT_VECTOR_SIZE ,
+                index_threshold=self.config.VECTOR_DB_THRESHOLD_INDEXING
+            )        
+        return None 
