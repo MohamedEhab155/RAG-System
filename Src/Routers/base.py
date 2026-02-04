035ea1df7eaeb3f50db0838effd5a95353fe998f
mohamedehab <github_user@ragsystem.w3w55cnj2gxutdvpccq3wjwucb.bx.internal.cloudapp.net>
2026-02-04 12:12:53 +0000

    fake line

--- Src/Routers/base.py
+++ Src/Routers/base.py
@@ -4,7 +4,8 @@ from Helper.config import get_settings, Settings
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"],
-)
+) 
+
 
 @base_router.get("/")
 async def welcome(app_settings: Settings = Depends(get_settings)):
